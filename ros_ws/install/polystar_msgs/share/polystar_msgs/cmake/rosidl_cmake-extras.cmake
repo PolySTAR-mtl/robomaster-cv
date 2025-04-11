db9d8d2c9882db94473b3e3ac4c6ef81d633b033
Sebastien Darche <sebastien.darche@polymtl.ca>
2025-04-11 15:26:01 -0400

    Clean up working tree

--- ros_ws/install/polystar_msgs/share/polystar_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(polystar_msgs_IDL_FILES "msg/HP.idl;msg/SwitchOrder.idl;msg/Target.idl;msg/Movement.idl;msg/GameStatus.idl;msg/GameStage.idl;msg/TurretFeedback.idl;msg/PositionFeedback.idl;msg/Shoot.idl")
-set(polystar_msgs_INTERFACE_FILES "msg/HP.msg;msg/SwitchOrder.msg;msg/Target.msg;msg/Movement.msg;msg/GameStatus.msg;msg/GameStage.msg;msg/TurretFeedback.msg;msg/PositionFeedback.msg;msg/Shoot.msg")
